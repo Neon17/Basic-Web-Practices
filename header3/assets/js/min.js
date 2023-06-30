11f06f1ecc6ec1c07ec896e629ac90d062ad136b
Neon17 <neon.neupane.9@gmail.com>
2023-06-30 17:44:26 +0545

    BUG FIX: At screen size <=768px There, navlists auto opened. So solved in min.js window.onload=function

--- header3/assets/js/min.js
+++ header3/assets/js/min.js
@@ -1,3 +1,11 @@
+window.onload = function() {
+    if (window.innerWidth<=768){
+        var showit = document.getElementById("NavIdCollapse");
+        showit.style.display = "none";
+        console.log("hello");
+    }
+    
+}
 function showNavlists () {
     if (window.innerWidth<=768){
         var showit = document.getElementById("NavIdCollapse");
